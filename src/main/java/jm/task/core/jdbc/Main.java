46fd4c3e85176b75b75bad75fffe9d318c01584d
Titanov23 <777titan16@gmail.com>
2023-06-18 21:56:15 +0300

    create UsersTable

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -18,7 +18,7 @@ public class Main {
 //        userService.removeUserById(1);
 
 //        userService.getAllUsers();
-//        userService.cleanUsersTable();
+        userService.cleanUsersTable();
 
 //        userService.dropUsersTable();
     }
